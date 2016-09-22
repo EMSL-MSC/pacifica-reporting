9004c50c09dcc8d2255aa7efac241e7e9ab35b53
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-09-22 10:58:20 -0700

    More Travis tweaks

--- test-everything.sh
+++ test-everything.sh
@@ -2,4 +2,4 @@
 
 # basic syntax check against all php files
 #find -name '*.php' | xargs -n 1 php -l
-find application/ -iname '*.php' -and ! \( -ipath '*libraries*' -or -ipath '*migrations*' \) | xargs -n 1 phpcs -n -s --standard=myemsl_ruleset.xml
+find -ipath '*application*' -and -iname '*.php' -and ! \( -ipath '*libraries*' -or -ipath '*migrations*' \) | xargs -n 1 phpcs -n --standard=myemsl_ruleset.xml
