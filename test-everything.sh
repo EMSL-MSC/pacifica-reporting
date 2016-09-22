f230be454067e0e3eeec5675d97f8330a67ecabb
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-09-22 11:29:31 -0700

    Maybe like this???

--- test-everything.sh
+++ test-everything.sh
@@ -2,4 +2,4 @@
 
 # basic syntax check against all php files
 #find -name '*.php' | xargs -n 1 php -l
-find -ipath '*application*' -and -iname '*.php' -and ! \( -ipath '*libraries*' -or -ipath '*migrations*' -or -ipath '*websystem*' \) | xargs -n 1 phpcs -n --standard=myemsl_ruleset.xml
+find -ipath './application*' -and -iname '*.php' -and ! \( -ipath '*libraries*' -or -ipath '*migrations*' -or -ipath '*websystem*' \) | xargs -n 1 phpcs -n --standard=myemsl_ruleset.xml
