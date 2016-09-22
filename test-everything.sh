51d6a1b5644e39eb1415fe9b57d9fb999d1e3006
Ken Auberry <kenneth.auberry@pnnl.gov>
2016-09-22 13:13:47 -0700

    One more

--- test-everything.sh
+++ test-everything.sh
@@ -3,4 +3,4 @@
 # basic syntax check against all php files
 #find -name '*.php' | xargs -n 1 php -l
 #find -iname '*.php' -and ! \( -ipath '*libraries*' -or -ipath '*migrations*' -or -ipath '*websystem*' \) | xargs -n 1 phpcs -n --standard=myemsl_ruleset.xml
-phpcs -n --extensions=php --ignore=*/websystem/*,*/system/*,*/migrations/*,*/libraries/* --standard=myemsl_ruleset.xml *
+phpcs -n --extensions=php --ignore=*/websystem/*,*/system/*,*/migrations/*,*/libraries/* --standard=myemsl_ruleset.xml application/
